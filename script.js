fc08ef429ba047b9a9555341a4b207bb7492bce4
d-serf <danserfass3@gmail.com>
2025-03-26 01:22:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ function showPage(pageId) {
     });
     document.getElementById(pageId).style.display = 'block';
 }
+
+document.getElementById('homeBtn').addEventListener('click', () => showPage('home'));
+document.getElementById('experienceBtn').addEventListener('click', () => showPage('experience'));
+document.getElementById('projectsBtn').addEventListener('click', () => showPage('projects'));
+document.getElementById('aboutBtn').addEventListener('click', () => showPage('about'));
